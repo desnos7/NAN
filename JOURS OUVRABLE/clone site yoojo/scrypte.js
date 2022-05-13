7178b48ab4176a5f4307358edf9f145713a43e16
desnos7 <beugremicaeldesnos@gmail.com>
2022-05-13 18:01:35 +0000

    mise a jour

---
+++ JOURS OUVRABLE/clone site yoojo/scrypte.js
@@ -0,0 +1,28 @@
+
+let connexion =document.querySelector('#connexion')
+let contener=document.querySelector('.contener')
+let modal= document.querySelector('.modal')
+let none =document.querySelector('#close')
+
+console.log(connexion)
+function show(){
+    modal.style.display='block';
+   contener.style.display='block';  
+   document.body.style.height='100%';
+    document.body.style.overflow='hidden'
+   
+
+
+}
+
+
+function hidden (){
+
+  modal.style.display='none';
+ contener.style.display='none';
+  document.body.style.overflow='none';
+
+}
+
+connexion.addEventListener('click',show);
+none.addEventListener('click',hidden);
